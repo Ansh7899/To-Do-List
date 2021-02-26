b03dab2e3ff8fec9f76c9ad2ef0a38c27bc0fd48
Ansh Kumar Gupta <63894658+Ansh7899@users.noreply.github.com>
2021-02-27 02:52:01 +0530

    Update app.js
    while using this use your own username and password

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-ansh:qGUzjDi7nX6vE76@cluster0.wcnlj.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://<your Username>:<Your Password>@cluster0.wcnlj.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
